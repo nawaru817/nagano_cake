1c9af223eae3a4583076c669fec4a9e98d691f76
nawaru817 <t010mbharada@gmail.com>
2021-03-11 10:55:22 +0000

    [Fix]商品登録画面修正完了

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -5,14 +5,16 @@ class Admin::ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.save
+    @item.save!
     redirect_to admin_items_path
   end
 
   def index
+    @items = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def destroy
