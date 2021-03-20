1d7776b8e70d3d2bfdeff97af789c07c45337ecd
nawaru817 <t010mbharada@gmail.com>
2021-03-20 07:36:09 +0000

    [Update] 商品詳細画面 更新

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5262f25330508765f88e85caa79a7ee278b344fb365449bd9ffe953cca9cb3f00eca754f4ddb9d340f071984a4eacd8cffd46d4443314e210d1d6bb4afb9c606'
