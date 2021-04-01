0aad0a713d0ad4d60931891fe1154bd993988f7f
nawaru817 <t010mbharada@gmail.com>
2021-04-01 23:50:27 +0000

    [Add] カート内画面

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,18 +1,33 @@
 class Public::CartItemsController < ApplicationController
-  
+
   def index
+    @cart_items = CartItem.all
+    @item_name = Item.pluck(:name)
+    @item_image = Item.pluck(:image_id)
+    @items = Item.all
+    @item_price = Item.pluck(:price)
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
+
   def destroy_all
   end
-  
+
   def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    @cart_item.save!
+    redirect_to cart_items_path
   end
-  
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :customer_id, :amount)
+  end
+
 end
